d4f069102253dcb9f6274af139001f8184eb826c
Ilija Tovilo <ilija.tovilo@me.com>
2026-03-25 16:54:32 +0100

    Fix incorrect RC-handling for ZEND_EXT_STMT op1
    Fixes GH-21504
    Closes GH-21528

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -1934,6 +1934,9 @@ static void zend_do_extended_stmt(znode* result) /* {{{ */
 
 	opline->opcode = ZEND_EXT_STMT;
 	if (result) {
+		if (result->op_type == IS_CONST) {
+			Z_TRY_ADDREF(result->u.constant);
+		}
 		SET_NODE(opline->op1, result);
 	}
 }
